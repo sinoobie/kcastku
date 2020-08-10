0c19df49f351d734a5690340194f555b6c03aea4
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2020-08-10 09:48:34 +0700

    Update Tampilan
    Sekarang list chapternya gw tampilin biar kagak bingung:)

--- kcast.py
+++ kcast.py
@@ -19,23 +19,29 @@ def req_data():
 	hsil=[{"title":x.text.strip(), "url":x["href"]} for x in data]
 	return hsil
 
-def get_capter(url):
+def get_capter(url, nopil):
 	_cap=[]
 	req=requests.get(url,headers=head)
 	bs=BS(req.text, 'html.parser')
 	data=bs.find_all('span', {'class':'leftoff'})
+
+	all=data[nopil-1].text.split(' ')[1]
+	if '-' in all:
+		n=int(all.split('-')[0])
+	elif '.' in all:
+		n=int(all.split('.')[0])
+	else:
+		n=int(all)
 	for x in data:
 		nc=x.text.strip().split(' ')[1]
-		if '-' in nc:
-			ncp=float(nc.split('-')[0])
-		else:
-			ncp=float(nc)
-		_cap.append([ncp, x.find('a')['href']])
+		_cap.append([n, nc, x.find('a')['href']])
+		n-=1
 	_cap.sort()
-	hsil=[{'cap':y[0], 'url':y[1]} for y in _cap]
+#	print(_cap)
+	hsil=[{'cap':y[1], 'url':y[2]} for y in _cap]
 	return hsil
 
-def download(url, title, nochap, noimg):
+def download(url, path, noimg):
 	hder={
 		'Host':'cdn.komikcast.com',
 		'user-agent':'Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.116 Mobile Safari/537.36 EdgA/45.06.4.5043',
@@ -43,31 +49,33 @@ def download(url, title, nochap, noimg):
 		}
 	r = requests.get(url, headers=hder, stream=True)
 	total_size = int(r.headers.get('content-length', 0))
-	print(f"\n# Downloading '{title}/chapter{nochap}'")
+	print(f"\n# Downloading \"{path.replace(f'{MAINDIR}/','')}\"")
 	print(f"[{url}]")
 	block_size = 1024
 	t=tqdm(total=total_size, unit='iB', unit_scale=True)
-	with open(f'{MAINDIR}/{title}/chapter{nochap}/'+'{:02}'.format(noimg)+'.jpg','wb') as f:
+	with open(f'{path}/'+'{:02}'.format(noimg)+'.jpg','wb') as f:
 		for data in r.iter_content(chunk_size=block_size):
 			if data:
 				t.update(len(data))
 				f.write(data)
 	t.close()
 
-def get_udl(url, title, nochap):
+def get_udl(data, url, title, nocap):
 	try:
 		os.mkdir(f"{MAINDIR}/{title}")
 	except: pass
 	try:
-		os.mkdir(f'{MAINDIR}/{title}/chapter{nochap}')
+		os.mkdir(f'{MAINDIR}/{title}/chapter{data[nocap-1]["cap"]}')
 	except: pass
+
+	path=f'{MAINDIR}/{title}/chapter{data[nocap-1]["cap"]}'
 	req=requests.get(url, headers=head)
 	bs=BS(req.text, 'html.parser')
 	imgs=bs.find_all('img', {'style':'display:none;visibility:hidden;'})
 	n=1
 	for i in imgs:
 		if 'cdn.komikcast.com' in i['data-cfsrc']:
-			download(i['data-cfsrc'], title, nochap, n)
+			download(i['data-cfsrc'], path, n)
 			n+=1
 
 def chap_dl(data, lih, title):
@@ -78,11 +86,11 @@ def chap_dl(data, lih, title):
 		else:
 			pil=range(int(plih[0]), len(data)+1)
 	else:
-		get_udl(data[int(lih)-1]['url'], title, int(lih))
+		get_udl(data, data[int(lih)-1]['url'], title, int(lih))
 		return True
 
 	for x in pil:
-		get_udl(data[x-1]['url'], title, x)
+		get_udl(data, data[x-1]['url'], title, x)
 
 
 
@@ -114,7 +122,7 @@ tit=[]
 inp=input("Cari: ")
 for x in data:
 	if inp.lower() in x['title'].lower():
-		print(f"{n}. {x['title']}")
+		print(f"[{n}] {x['title']}")
 		url.append(x['url'])
 		tit.append(x['title'])
 		n+=1
@@ -123,12 +131,15 @@ if len(tit) == 0:
 else:
 	pil=int(input("Pilih: "))
 
-cap=get_capter(url[pil-1])
-print(f"\n\t\033[96m[{len(cap)} Chapter ditemukan]\n\n[Note] Mungkin jumlah chapternya terlihat lebih banyak atupun lebih sedikit dari pada aslinya, itu karena jika ada chapter (misal: 10.5) chapter tsb akan menjadi chapter11 (dibulatkan keatas). ataupun jika ada chapter gabungan (misal: 14-15) kedua chapter ini akan dijadikan satu nama (chapter14)\n")
+print(f"\n\033[96m[•{tit[pil-1]}•]")
+cap=get_capter(url[pil-1], pil)
+for x in range(len(cap)):
+	print(f"[{x+1}] Chapter {cap[x]['cap']}")
+print(f"[ {len(cap)} (Total) Chapter ditemukan ]\n")
 lih=input("""\033[97m[info]
-# ketik (misalnya: 10-) untuk mendownload dari chapter 10 sampai akhir chapter
-# ketik (misalnya: 10-20) untuk mendownload dari chapter 10 sampai chapter 20
-# ketik angka saja tanpa garis untuk mendownload salah satu chapter
-noobie_> """)
+# ketik (misalnya: 10-) untuk mendownload dari nomor 10 sampai akhir nomor
+# ketik (misalnya: 10-20) untuk mendownload dari nomor 10 sampai nomor 20
+# ketik angka saja tanpa garis untuk mendownload salah satu nomor
+noobie/> """)
 chap_dl(cap, lih, tit[pil-1])
 print("\n\033[92m[all done] Semua hasil downloadnya tesimpan di internal storage (/sdcard/kcastku)")
